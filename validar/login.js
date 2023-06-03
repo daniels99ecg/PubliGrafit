86cab9ef4bd80dabc82b9970e19c24ef07c5994b
daniel <emi--lio--@hotmail.com>
2023-06-02 20:12:30 -0500

    Organizando errores

--- validar/login.js
+++ validar/login.js
@@ -9,15 +9,15 @@ const contra=document.getElementById('contrasena').value;
 
 if(usuario=="danielsenju1999@gmail.com" && contra=="123"){
     console.log('ingreso')
-    window.location='PubliGrafit/../dashboard.html';
+    window.location='../PubliGrafit/dashboard.html';
 }
 
 else if(usuario=="camilo@gmail.com" && contra=="123456") {
     console.log('ingreso')
-    window.location='./Empleado/dashboard.html';
+    window.location='../PubliGrafit/Empleado/dashboard.html';
 }else{
     console.log('no ingreso')
-    window.location='PubliGrafit/../login.html';
+    window.location='../PubliGrafit/login.html';
 
 }
 
