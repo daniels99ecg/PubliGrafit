f84ffa7ef4f77815968f67b2fd31a0e4b4853aa6
daniel <emi--lio--@hotmail.com>
2023-06-13 18:52:16 -0500

    Agregando validaciones

--- validar/validar.js
+++ validar/validar.js
@@ -3,13 +3,17 @@ function validart(){
     let vdni=document.getElementById("dni").value;
     let vnombre=document.getElementById("nombre").value;
     let vapellido=document.getElementById("apellido").value;
-    let vtelefono=document.getElementById("telefono").value;
+    
     let vcorreo=document.getElementById("correo").value;
     let vcontrasena=document.getElementById("contrasena").value;
+    let vconfirmar=document.getElementById("confirmar").value;
+
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
+
     
-    
-    if(vdni=="" || vnombre=="" || vapellido==""||vtelefono==""||vcorreo==""||vcontrasena==""){
-        tabla=["#dni", "#nombre","#apellido", "#telefono", "#correo", "#contrasena"];
+    if(vdni=="" || vnombre=="" || vapellido==""||vcorreo==""||vcontrasena==""||vconfirmar==""){
+
+        tabla=["#dni", "#nombre","#apellido", "#correo", "#contrasena", "#confirmar"];
         for (const i in tabla) {
            document.querySelector(tabla[i]).style.borderColor="red";
            document.querySelector(tabla[i]).placeholder="Campo Requerido";
@@ -17,8 +21,24 @@ function validart(){
 
 
       
-    }else{
-     console.log("Pruebas")
+    }else if(emailRegex.test(vcorreo.value)){
+        document.querySelector('#correo').style.borderColor="red";
+        document.querySelector('#correo').value="Ingrese un correo valido";
+    }else if(vcontrasena!==vconfirmar){
+       tabla=["#contrasena","#confirmar" ];
+       for (const i in tabla) {
+        document.querySelector(tabla[i]).style.borderColor="red";
+        document.querySelector(tabla[i]).value="Las contraseñas no son iguales";
+     }
+
+    }
+    
+    else{
+        tabla=["#dni", "#nombre","#apellido", "#correo", "#contrasena", "#confirmar"];
+        for (const i in tabla) {
+           document.querySelector(tabla[i]).style.borderColor="green";
+         
+        }
     }
 
   
